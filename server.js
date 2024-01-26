f69aaf87164abcf9e96e97d05a4a0f5764789f68
GeorgeOprea <oprea.george14@yahoo.com>
2024-01-26 12:17:56 +0200

    Update server for routing and redirects

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const stripe = require("stripe")(
   "sk_test_51ObP5JDnlYS1GOoDQqQklWSLProycuXgObRBvkfV49kFM3yQ4xqrP0rnWTukNfSUnZsBMdYx5fBX1tgkjDXyRkbQ00HCLAS2Eo"
 );
@@ -7,6 +8,20 @@ const stripe = require("stripe")(
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get(/^\/(?!success|cancel).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.get("/success", (req, res) => {
+  res.redirect("https://chic-trend-boutique.onrender.com/success");
+});
+
+app.get("/cancel", (req, res) => {
+  res.redirect("https://chic-trend-boutique.onrender.com/cancel");
+});
 
 app.post("/checkout", async (req, res) => {
   const items = req.body.items;
@@ -31,7 +46,7 @@ app.post("/checkout", async (req, res) => {
   });
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
