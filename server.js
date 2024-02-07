7ed63540184a73ae41783eeb86906e17f579485e
GeorgeOprea <oprea.george14@yahoo.com>
2024-02-07 13:36:55 +0200

    remove express static

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
 const app = express();
-app.use(cors());
 app.use(express.json());
-app.use(express.static("boutique"));
 
 const corsOptions = {
   origin: `${process.env.CLIENT_URL}`,
